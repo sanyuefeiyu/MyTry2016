631d01f3b3a105ea996f1de6ee188946873a14f5
double <shishuanghe@gmail.com>
2016-08-31 00:45:16 +0800

    optimize make file.

--- build/android/build.bat
+++ build/android/build.bat
@@ -8,7 +8,16 @@ echo.
 %MAKE_PATH%
 
 echo.
-echo build project finish
+
+set error=%errorlevel%
+if %error%==0 (
+    echo build project success
+) else (
+    cmd /cecho [1;31m
+    echo build project fail and error code is %error%
+    cmd /cecho [0;40m
+)
+
 echo.
 
 pause
